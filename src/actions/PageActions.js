aa71b1edfef367adfbca72dae5769442c870d45d
rize <pavlov.v@wlbl.ru>
2018-03-05 13:22:15 +0300

    исправлено кеширование картинок

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -9,62 +9,27 @@ const VK = window.VK;
 if (!VK)
 	throw new Error('ошибка загрузки вк апи');
 
-
-let photosArr = [];
-let cached = false;
+export let photosObj = {
+	photosArr: [],
+	cached: false
+};
 
 
 export function getPhotos(year) {
-	return dispatch => {
+	return function(dispatch) {
 		dispatch({
 			type: GET_PHOTOS_REQUEST,
 			payload: year
 		});
 
-		if (cached) {
-			let photos = makeYearPhotos(photosArr, year);
+		if (photosObj.cached) {
+			let photos = makeYearPhotos(photosObj.photosArr, year);
 			dispatch({
 				type: GET_PHOTOS_SUCCESS,
 				payload: photos
 			});
 		} else {
-			console.log(dispatch);
-			getMorePhotos(photosArr, cached, 0, 200, year, dispatch);
+			getMorePhotos(0, 200, year, dispatch);
 		}
-
-
-		// VK.Auth.getLoginStatus(r => {
-		// 	if (r.session) {
-		// 		VK.Api.call('photos.getAll', {
-		// 			user_ids: r.session.mid,
-		// 			count: 20,
-		// 			extended: true,
-		// 			v: 5.73
-		// 		},
-		// 		r => {
-		// 			const photos = r.response.items;
-		//
-		// 			if (photos) {
-		// 				dispatch({
-		// 					type: GET_PHOTOS_SUCCESS,
-		// 					error: '',
-		// 					payload: photos
-		// 				});
-		// 			} else {
-		// 				dispatch({
-		// 					type: GET_PHOTOS_FAIL,
-		// 					payload: new Error('Ошибка загрузки фотографий'),
-		// 					error: true
-		// 				});
-		// 			}
-		// 		});
-		// 	} else {
-		// 		dispatch({
-		// 			type: GET_PHOTOS_FAIL,
-		// 			payload: new Error('Ошибка загрузки фотографий'),
-		// 			error: true
-		// 		});
-		// 	}
-		// });
 	}
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,6 +2,7 @@ import {
 	GET_PHOTOS_SUCCESS,
 	GET_PHOTOS_FAIL
 } from '../constants/Page';
+import { photosObj } from '../actions/PageActions';
 
 const VK = window.VK;
 if (!VK)
@@ -24,7 +25,7 @@ export function makeYearPhotos(photos, selectedYear) {
 }
 
 
-export function getMorePhotos(photosArr = [], cached, offset, count, year, dispatch) {
+export function getMorePhotos(offset, count, year, dispatch) {
 	const user_ids = VK.Auth.getSession().mid;
 	VK.Api.call('photos.getAll', {
 		extended: true, v: 5.73, user_ids, count, offset
@@ -32,22 +33,23 @@ export function getMorePhotos(photosArr = [], cached, offset, count, year, dispa
 		try {
 			if (offset <= r.response.count - count) {
 				offset += 200;
-				photosArr = photosArr.concat(r.response.items);
-				getMorePhotos(photosArr, cached, offset, count, year, dispatch);
+				photosObj.photosArr = photosObj.photosArr.concat(r.response.items);
+				getMorePhotos(offset, count, year, dispatch);
 			} else {
-				let photos = makeYearPhotos(photosArr, year);
-				cached = true;
+				let photos = makeYearPhotos(photosObj.photosArr, year);
+				photosObj.cached = true;
 				dispatch({
 					type: GET_PHOTOS_SUCCESS,
 					payload: photos
 				});
 			}
 		} catch(e) {
-			console.error(r.error.error_msg)
+			const error = r.error.error_msg || e;
+			console.error(error);
 			dispatch({
 				type: GET_PHOTOS_FAIL,
 				error: true,
-				payload: new Error(r.error.error_msg)
+				payload: new Error(error)
 			});
 		}
 	});
